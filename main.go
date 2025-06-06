253340f7df4c6fbaf11a2d56121155e62fb4c8b9
aamiranwar <aamirr.anwarr@gmail.com>
2025-06-05 21:58:02 -0400

    Solve basic XOR

--- main.go
+++ main.go
@@ -4,6 +4,26 @@ import (
 	"fmt"
 )
 
+func singleNumber(nums []int) int {
+	if len(nums) <= 0 {
+		return 0
+	}
+
+	if len(nums) == 1 {
+		return nums[0]
+	}
+
+	x := nums[0]
+	for i := 1; i < len(nums); i++ {
+		x = x ^ nums[i]
+	}
+	return x
+}
+
 func main() {
 	fmt.Println("Hello world!")
+	input := []int{1}
+	ret := singleNumber(input)
+	fmt.Println("Return value of x = ", ret)
+
 }
