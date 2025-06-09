057f180f0569a81143b71457e24286a9ad7f9dae
packapun <packapun.dev@gmail.com>
2025-06-09 17:56:43 -0400

    [LC] Reverse int32

--- main.go
+++ main.go
@@ -4,6 +4,28 @@ import (
 	"fmt"
 )
 
+func reverse(x int) int {
+	fmt.Println("Reversing", x)
+	reversedNum := 0
+	isNegative := x < 0
+	if isNegative {
+		x = -x
+	}
+
+	for x > 0 {
+		reversedNum = reversedNum*10 + x%10
+		if reversedNum > math.MaxInt32 {
+			return 0
+		}
+		x = x / 10
+	}
+	if isNegative {
+		return -reversedNum
+	}
+	return reversedNum
+}
+
+
 func main() {
 	fmt.Println("Hello world! This is the main driver program")
 }
